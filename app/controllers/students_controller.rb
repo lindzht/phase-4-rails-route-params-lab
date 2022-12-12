ba87d4aaf58039d8bceb214dd14de97f2a9b2a69
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-12-12 12:26:45 -0500

    commit tree e74af3ed71da13418b005530eca336c9fa5a8221

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,17 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    # students = Student.search_for_student(params)
+    students = Student.all.select do |student|
+      student.first_name == (params[:first_name]).capitalize() || student.last_name == (params[:last_name]).capitalize()
+    end
     render json: students
+
+  end
+
+  def find_student
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,14 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  # def self.search_for_student(params)
+  #   students = self.all.map do |student|
+  #     if student.first_name == (params[:first_name]) || student.last_name == (params[:last_name])
+  #       student
+  #     else
+  #       ""
+  #     end
+  #   end
+  # end
+
 end
